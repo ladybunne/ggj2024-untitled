3465ceb50f0d7051754673f4c36157f1e9355fc4
Chris Chua <someone42@gmail.com>
2024-01-27 02:10:08 +1100

    Add Bug which locks the camera

---
+++ CodeStuff/LockCamera.gd
@@ -0,0 +1,8 @@
+class_name LockCamera extends BugEntity
+
+func _input(event):
+	super._input(event)
+	if player_inside != null and event.is_action_pressed("player_interact"):
+		var camera = player.find_child("RemoteTransform2D") as RemoteTransform2D
+		if camera != null:
+			camera.update_position = false
