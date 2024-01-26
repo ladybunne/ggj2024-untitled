124539847d7a78132b8f3350e1f6c3eaf056d503
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-27 09:45:07 +1100

    talk to EnableFly bug again to re-enable gravity

--- CodeStuff/EnableFly.gd
+++ CodeStuff/EnableFly.gd
@@ -1,8 +1,17 @@
 class_name EnableFly extends BugEntity
 
-@export var flyComponent: Fly 
+@export var flyComponent: Fly
+@export var flying_enabled: bool = false
+var jump_component_cache: JumpComponent = null
 
 func _input(event):
 	super._input(event)
 	if player_inside != null and event.is_action_pressed("player_interact"):
-		player.jump_component = flyComponent
+		if flying_enabled:
+			flying_enabled = false
+			player.jump_component = jump_component_cache
+			jump_component_cache = null
+		else:
+			jump_component_cache = player.jump_component
+			player.jump_component = flyComponent
+			flying_enabled = true
