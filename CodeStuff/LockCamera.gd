b6c572ea4f7f49d4972413b01423a5a39a6c4865
Chris Chua <someone42@gmail.com>
2024-01-27 09:51:17 +1100

    Talk to the LockCamera bug again to toggle camera lock on/off

--- CodeStuff/LockCamera.gd
+++ CodeStuff/LockCamera.gd
@@ -5,4 +5,4 @@ func _input(event):
 	if player_inside != null and event.is_action_pressed("player_interact"):
 		var camera = player.find_child("RemoteTransform2D") as RemoteTransform2D
 		if camera != null:
-			camera.update_position = false
+			camera.update_position = !camera.update_position
