6ddada584ba0a4a5bb56425263e63e0a81c02376
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-27 10:50:01 +1100

    ElderBug now only follows you after being talked to and has dialogue hooked up
    dialogue itself is TODO

--- CodeStuff/ElderBug.gd
+++ CodeStuff/ElderBug.gd
@@ -2,17 +2,17 @@ extends BugEntity
 
 @export var character_body: CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	super._ready()
 	print("Init Elder bug")
-	# TEMP: this should be triggered when we talk to him but for
-	# 		now I just activate it immediately
-	character_body.is_following = true 
+	character_body.is_following = false 
 
 func give_player_ref(p_player: PlatformerCharacter):
 	super(p_player)
 	character_body.player = p_player
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	super._input(event)
+	if player_inside != null and event.is_action_pressed("player_interact"):
+		print("Interacted with ElderBug")
+		character_body.is_following = true
