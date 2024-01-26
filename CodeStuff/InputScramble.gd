0f0c9b2b494c91b4cd81a33d9e8b4f8486a4bd22
Chris Chua <someone42@gmail.com>
2024-01-26 23:37:38 +1100

    Clarify that the input scrambler implementation is how we're doing things

--- CodeStuff/InputScramble.gd
+++ CodeStuff/InputScramble.gd
@@ -5,11 +5,16 @@ class_name InputScramble extends BugEntity
 func _input(event):
 	super._input(event)
 	if player_inside != null and event.is_action_pressed("player_interact"):
-		# TODO: put this in platformer_character.gd
+		# Reaching inside player and setting/getting properties is very naughty
+		# but we don't want to refactor/modify platformer_character so this is
+		# the only way to shuffle the input keys
 		for i in range(len(move_names)):
 			var temporarySwap = player.get(move_names[i])
 			var target = randi_range(0, len(move_names) - 1)
 			player.set(move_names[i], player.get(move_names[target]))
 			player.set(move_names[target], temporarySwap)
-		# TODO: definitely put this in platformer_character.gd
+		# Need to call init_run_component()/init_jump_component() to
+		# reinitialise move_left_name/move_right_name in the run/jump
+		# components. An extra naughty way to do this without refactoring
+		# platformer_character is to just call _ready() again :-)
 		player._ready()
