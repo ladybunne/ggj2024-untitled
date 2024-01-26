89a96d41207b905f061fd71be80a72eaf24abbf0
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-26 21:56:49 +1100

    give_player_ref

--- CodeStuff/BugEntity.gd
+++ CodeStuff/BugEntity.gd
@@ -8,10 +8,17 @@ signal goal_satisfied
 
 var player_inside: PlatformerCharacter
 
+# Scene controller gives every bug a ref to the player
+var player: PlatformerCharacter
+
 func _ready():
 	if collision_area != null:
 		collision_area.body_entered.connect(player_entered)
 		collision_area.body_exited.connect(player_exited)
+
+func give_player_ref(p_player: PlatformerCharacter):
+	player = p_player
+	
 
 func player_entered(body: Node2D):
 	var player := body as PlatformerCharacter
