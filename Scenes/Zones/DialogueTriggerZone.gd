83a2bcd848719c569f8e272af0aff253504a814d
LarrikinDan <daniel@rona.id.au>
2024-01-26 23:58:39 +1100

    Dialogue Trigger Zone
    Still missing one-shot capability but that's easy.

---
+++ Scenes/Zones/DialogueTriggerZone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var dialogue_to_trigger: DialogueData
+@export var one_shot: bool = true
+@export var dialogue_manager: DialogueManager
+
+func _enter_tree():
+	dialogue_manager = get_tree().get_first_node_in_group("DialogueManager")
+	pass
+
+func _on_body_entered(body):
+	dialogue_manager.data = dialogue_to_trigger
+	#if one_shot then delete this area.
+	pass # Replace with function body.
