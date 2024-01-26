5e5b68fe443ff40c0707af3565798261e811bee9
Ladybunne <github@ladybunne.net>
2024-01-26 22:00:19 +1100

    Add Player reference to all bugs! Remember 2 add your thing to BugGroup!

--- CodeStuff/SceneController.gd
+++ CodeStuff/SceneController.gd
@@ -1,7 +1,12 @@
 class_name SceneController extends Node
 
+@export var player: PlatformerCharacter
+
 func _ready():
 	# Grab all bugs and listen for goal_completion
+	
+	# Give all bugs a reference to player
+	get_tree().call_group("BugGroup", "give_player_ref", player)
 	pass
 	
 func on_bug_goal_completed():
