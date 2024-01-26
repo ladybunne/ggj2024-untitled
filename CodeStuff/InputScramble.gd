104313d1d9545f2aaab24f5a147be1904db876ae
Chris Chua <someone42@gmail.com>
2024-01-27 09:48:53 +1100

    Talk to InputScrambler to toggle input randomisation on/off

--- CodeStuff/InputScramble.gd
+++ CodeStuff/InputScramble.gd
@@ -2,19 +2,33 @@ class_name InputScramble extends BugEntity
 
 @export var move_names: Array[String]
 
+var scrambled: bool
+var original_move_names: Array[String]
+
 func _input(event):
 	super._input(event)
 	if player_inside != null and event.is_action_pressed("player_interact"):
-		# Reaching inside player and setting/getting properties is very naughty
-		# but we don't want to refactor/modify platformer_character so this is
-		# the only way to shuffle the input keys
-		for i in range(len(move_names)):
-			var temporarySwap = player.get(move_names[i])
-			var target = randi_range(0, len(move_names) - 1)
-			player.set(move_names[i], player.get(move_names[target]))
-			player.set(move_names[target], temporarySwap)
-		# Need to call init_run_component()/init_jump_component() to
-		# reinitialise move_left_name/move_right_name in the run/jump
-		# components. An extra naughty way to do this without refactoring
-		# platformer_character is to just call _ready() again :-)
-		player._ready()
+		if !scrambled:
+			# Save existing inputs
+			original_move_names = []
+			for i in range(len(move_names)):
+				original_move_names.append(player.get(move_names[i]))
+			# Reaching inside player and setting/getting properties is very naughty
+			# but we don't want to refactor/modify platformer_character so this is
+			# the only way to shuffle the input keys
+			for i in range(len(move_names)):
+				var temporarySwap = player.get(move_names[i])
+				var target = randi_range(0, len(move_names) - 1)
+				player.set(move_names[i], player.get(move_names[target]))
+				player.set(move_names[target], temporarySwap)
+			# Need to call init_run_component()/init_jump_component() to
+			# reinitialise move_left_name/move_right_name in the run/jump
+			# components. An extra naughty way to do this without refactoring
+			# platformer_character is to just call _ready() again :-)
+			player._ready()
+		else:
+			# Unscramble by restoring original names
+			for i in range(len(move_names)):
+				player.set(move_names[i], original_move_names[i])
+			player._ready()
+		scrambled = !scrambled
