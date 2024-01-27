527f77a24b86d010352bca6fba1c718363b36dc2
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-27 20:30:53 +1100

    timer for repel bug label is working :)

--- CodeStuff/RepelBug.gd
+++ CodeStuff/RepelBug.gd
@@ -1,1 +1,20 @@
 extends BugEntity
+
+
+func _on_yelling_timer_timeout():
+	hide_yell()
+	
+func _on_between_yelling_timer_timeout():
+	show_yell()
+
+func show_yell():
+	var label = get_parent().get_node("YellingLabel")
+	label.visible = true
+	var yelling_timer: Timer = get_node("YellingTimer")
+	yelling_timer.start()
+
+func hide_yell():
+	var label = get_parent().get_node("YellingLabel")
+	label.visible = false
+	var between_yelling_timer: Timer = get_node("BetweenYellingTimer")
+	between_yelling_timer.start()
