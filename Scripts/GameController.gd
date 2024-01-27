f6aaae35f9e559d5861737744a4ead93c0ddd6c6
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-28 10:21:46 +1100

    play music

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _ready():
-	AudioManager.play_music("Game Music")
+	#AudioManager.play_music("Game Music")
+	pass
 
 func _on_button_pressed():
 	FlowController.load_scene("Main Menu")
