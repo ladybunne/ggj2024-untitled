f02af30b2d85f01a96824d075954eebd3f8d3eda
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-27 15:09:03 +1100

    port commits over for lonely colliders scene

--- CodeStuff/CollisionBoxTooBigBug.gd
+++ CodeStuff/CollisionBoxTooBigBug.gd
@@ -11,5 +11,3 @@ func _process(delta):
 
 func _input(event):
 	super._input(event)
-	if platformer_inside != null and event.is_action_pressed("player_interact"):
-		print("Interacted with CollisionBoxTooBig")
--- bunne-gdkit/Systems/PlatformerCharacter/platformer_character.gd
+++ bunne-gdkit/Systems/PlatformerCharacter/platformer_character.gd
@@ -231,6 +231,13 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
+	var push_force = 80.0
+	# Resolve collisions
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+	
 	update_animation_state()
 
 	position_cache = position
