00532ef256440b9eeeea65d8cd63775ec812c3a1
LarrikinDan <daniel@rona.id.au>
2024-01-27 21:52:06 +1100

    Elder Bug Moves Like A Champion

--- CodeStuff/ElderBug.gd
+++ CodeStuff/ElderBug.gd
@@ -20,7 +20,6 @@ func give_player_ref(p_player: Player):
 func _input(event):
 	super._input(event)
 	if platformer_inside != null and event.is_action_pressed("player_interact"):
-		print("Interacted with ElderBug")
 		character_body.is_following = true
 
 func _process(delta):
