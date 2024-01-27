a4c7e278e2d0fc7b0443998bcad4dc5682526fe0
Ladybunne <github@ladybunne.net>
2024-01-27 15:44:56 +1100

    Tweak background removing

--- CodeStuff/RemoveBackground.gd
+++ CodeStuff/RemoveBackground.gd
@@ -8,5 +8,5 @@ func _input(event):
 func toggle_remove_background():
 	if scene_controller.ui_state != scene_controller.UIState.DIALOGUE:
 		return
-	# Toggle canvas layer 1 (background) on/off
-	get_viewport().canvas_cull_mask ^= 1
+	# Toggle canvas layer 5 (background) on/off
+	get_viewport().set_canvas_cull_mask_bit(4, !get_viewport().get_canvas_cull_mask_bit(4))
